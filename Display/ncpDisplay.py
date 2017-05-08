ff5886f7d958b3fbe1f6fb35415eed2fe5b45e73
chb50 <cedricblake48@gmail.com>
2017-05-08 18:59:23 +0000

    final version

--- Display/ncpDisplay.py
+++ Display/ncpDisplay.py
@@ -4,10 +4,11 @@ from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
 
-fileName = "logfile.txt"
+fileName = "logfile.txt" #name of file written by server
 
 messages = 3 #number of messages that are displayed at once
 
+#function for comparing the length of 3 strings
 def threeLenCmp(len1, len2, len3):
     if(len1 > len2):
         if(len1 > len3):
@@ -24,14 +25,17 @@ class RunText(SampleBase):
         super(RunText, self).__init__(*args, **kwargs)
 
     def Run(self):
+        #initialize message position
         offscreenCanvas = self.matrix.CreateFrameCanvas()
+        #initialize and set font size
         font = graphics.Font()
         font.LoadFont("../../fonts/7x13.bdf")
+        #set prefered text colors
         textColor1 = graphics.Color(255, 155, 0)
         textColor2 = graphics.Color(255, 0, 155)
         textColor3 = graphics.Color(0, 155, 255)
         pos = offscreenCanvas.width
-        fd = open(fileName, 'r')
+        fd = open(fileName, 'r') #open the log file
         #print(self.args)
         
         logLine1 = fd.readline() #attempt to read line, if end of file, "" is given
@@ -46,6 +50,8 @@ class RunText(SampleBase):
         logLine3 = logLine3[:-1]
         logLine3 = logLine3[:-1]
 
+        #this while loop will continuously check for new messages and will display the
+        #new messages as they are written to the log file by the server connection
         while True:
 
             offscreenCanvas.Clear()
@@ -55,6 +61,9 @@ class RunText(SampleBase):
 
             longLen = threeLenCmp(len1, len2, len3);
             pos -= 1
+            #this if/else statement is used to manage when we stop attempting to display to the matrix
+            #it tests for the message with the longest length, then prints up to that many characters
+            #this is so that messages do not get truncated during display
             if (longLen == 1):
                 if (pos + len1 < 0):
                     pos = offscreenCanvas.width
@@ -99,6 +108,7 @@ class RunText(SampleBase):
                     logLine3 = logLine3[:-1]
 
             time.sleep(0.05)
+            #reset message position bach to right side of display
             offscreenCanvas = self.matrix.SwapOnVSync(offscreenCanvas)
         fd.close()
 
